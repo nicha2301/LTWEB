cfc9c5e55d92d2d94fc77ee684064b4b947058f7
Nguyen Quoc Trung <nqt230103a@gmail.com>
2023-10-05 20:23:05 +0700

    Task 7

---
+++ Lap4/Task7/app.js
@@ -0,0 +1,64 @@
+const container = document.querySelector('.container-btn')
+const body = document.querySelector('body')
+const size = document.querySelector('input')
+
+
+function randomColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+
+function randomNum(max, min) {
+    let arr = [];
+    for (i = 0; i < max; i++) {
+        x = Math.floor(Math.random() * max) + min;
+        if (arr.includes(x) == true) {
+            i = i - 1;
+        } else {
+            if (x > max == false) {
+                arr.push(x);
+            }
+        }
+    }
+    return arr;
+}
+
+
+function handleEvent() {
+    const numbers = randomNum(size.value, 1);
+    let count = 1
+    let arr = [1]
+
+    for (let i = 0; i < size.value; i++) {
+        const wrapperButton = document.createElement('div')
+        const button = document.createElement('button')
+        button.classList.add('button')
+        button.classList.add('disabled')
+        button.classList.add('button-' + `${numbers[i]}`)
+        button.innerText = `${numbers[i]}`
+        button.style.backgroundColor = randomColor()
+        button.addEventListener('click', e => {
+            if (arr.includes(Number(e.target.innerText))) {
+                arr.push(++count)
+                e.target.style.display = 'none'
+                console.log(count)
+                console.log(arr)
+            } else {
+                alert('Wrong')
+            }
+        })
+        container.appendChild(wrapperButton)
+        wrapperButton.appendChild(button)
+    }
+}
+
+
+
+
+
+
+
