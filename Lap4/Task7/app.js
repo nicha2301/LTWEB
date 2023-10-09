2770f595afa003c811d84e1875490e0868de6e81
Nguyen Quoc Trung <nqt230103a@gmail.com>
2023-10-09 11:50:50 +0700

    update task 7

--- Lap4/Task7/app.js
+++ Lap4/Task7/app.js
@@ -1,7 +1,9 @@
 const container = document.querySelector('.container-btn')
+const gameover = document.querySelector('.fail')
 const body = document.querySelector('body')
 const size = document.querySelector('input')
-
+const step = document.querySelector('.tutorial-step')
+const chances = document.querySelector('.chance')
 
 function randomColor() {
     var letters = '0123456789ABCDEF';
@@ -33,7 +35,9 @@ function handleEvent() {
     const numbers = randomNum(size.value, 1);
     let count = 1
     let arr = [1]
+    let chance = 3
 
+    //create button
     for (let i = 0; i < size.value; i++) {
         const wrapperButton = document.createElement('div')
         const button = document.createElement('button')
@@ -43,23 +47,28 @@ function handleEvent() {
         button.innerText = `${numbers[i]}`
         button.style.backgroundColor = randomColor()
         button.addEventListener('click', e => {
+
+            //remove button when click it
             if (arr.includes(Number(e.target.innerText))) {
                 arr.push(++count)
                 e.target.style.display = 'none'
+                //tutorial next step
+                step.innerText = `Next step: ${arr[arr.length - 1]}`
+
             } else {
                 alert('Wrong')
+                //remaining chances
+                chances.innerText = `Remaining opportunities: ${chance - 1}`
+                chance = chance - 1
+                if (chance == 0) {
+                    gameover.style.display = 'flex'
+                }
             }
-            //remove button when all buttons were removed
+
+            //remove wrapper button when all buttons were removed
             if (arr.length == Number(size.value) + 1) {
                 alert("Congratulations")
-                //set original input after win the game
-                size.value = ''
-
-                let child = container.lastElementChild
-                while (child) {
-                    container.removeChild(child)
-                    child = container.lastElementChild
-                }
+                CleanUpField()
             }
         })
         container.appendChild(wrapperButton)
@@ -68,6 +77,25 @@ function handleEvent() {
 
 }
 
+function CleanUpField() {
+    //set original value input after win the game
+    size.value = ''
+    //set original value tutorial
+    step.innerText = ''
+    //set original value remaining opportunities
+    chances.innerText = ''
+
+    let child = container.lastElementChild
+    while (child) {
+        container.removeChild(child)
+        child = container.lastElementChild
+    }
+}
+
+function playAgain() {
+    gameover.style.display = 'none'
+    CleanUpField()
+}
 
 
 
